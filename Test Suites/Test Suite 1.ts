a42dafb81a6a0c0cdea83f7dfdd53280b4374187
Ben rhouma <hajer.benrhouma@altyx.io>
2019-12-27 13:04:01 +0100

    Test suite 1

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4b127c4f-80a7-440c-b74a-acf734ae4377</testSuiteGuid>
+   <testCaseLink>
+      <guid>2bbd5023-bae7-4bf9-9314-06333cb23547</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Ali</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44a6263f-4e3d-4dbe-93a1-6c2cc2f5eafe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Ines</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
